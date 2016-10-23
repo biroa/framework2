6219361fcc9bc3e3683d4a1319bdcde2b862c18c
biroa <adam.biro@gmail.com>
2016-10-23 20:50:36 +0100

    add routing functionality

--- Core/Router.php
+++ Core/Router.php
@@ -1,24 +1,70 @@
 <?php
 
-/**
-*
-* Router
-*
-*/
 
+/**
+ * Class Router
+ */
 class Router
 {
 
+    /**
+     * Associative array of routing table
+     * @var array
+     */
     protected $routes = [];
 
+    /**
+     * Matched route parameters
+     * @var array
+     */
+    protected $params = [];
+
+    /**
+     * @param string $route
+     * @param array $params
+     *
+     * @return void
+     */
     public function add($route, $params)
     {
-	$this->routes[$route] = $params;
+        $this->routes[$route] = $params;
     }
 
+    /**
+     * Get the routes from the routing table
+     *
+     * @return array
+     */
     public function getRoutes()
     {
-	return $this->routes;
+        return $this->routes;
     }
 
+    /**
+     * Match the routes to the route in the routing table, setting the $params
+     * property if a route is found.
+     *
+     * @param string $url The route URL
+     * @return boolean true if a match is found, false otherwise
+     */
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the currently matched parameters
+     * @return array
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,11 +1,22 @@
-<?php
-
-//Front Controller
-
-//echo 'Requested URL = ' . $_SERVER['QUERY_STRING'];
+<?php //Front Controller
 
 require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/news', ['controller' => 'Posts', 'action' => 'news']);
+
+//echo '<pre>';
+//var_dump($router->getRoutes());
+//echo '</pre>';
+
+$url = $_SERVER['QUERY_STRING'];
+if($router->match($url)){
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+}else{
+    echo 'No route found for URL' . $url;
+}
